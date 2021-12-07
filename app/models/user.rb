55a87df1a47c1a1c6e9f564f3d74f444cd409765
quanpx <quanphungxuan00@gmail.com>
2021-12-07 17:23:22 +0000

    Add profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-        has_one_attached :avatar
-
-  validates :name, presence: true
-  validates :age, presence: true
 end
