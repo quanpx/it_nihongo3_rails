5bfc360829f611cfbe595375f4bac688748bc4fe
quanpx <quanphungxuan00@gmail.com>
2021-12-07 17:38:29 +0000

    add name age to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  validates :name, presence: true
+  validates :age, presence: true
 end
